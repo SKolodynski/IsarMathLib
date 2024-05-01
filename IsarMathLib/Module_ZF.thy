a618601e46153e8c71efd3d29b8fc5d5195ec002
dnl232 <danconsa@hotmail.com>
2024-05-01 10:46:36 +0200

    Module_ZF_1.thy - Submodules - Linear combinations

--- IsarMathLib/Module_ZF.thy
+++ IsarMathLib/Module_ZF.thy
@@ -283,8 +283,7 @@ lemma (in module0) mult_zero:
   assumes "g\<in>\<M>"
   shows "\<zero> \<cdot>\<^sub>S g=\<Theta>"
 proof-
-  from vec_act_homo.homomor_dest_zero have "H ` \<zero> = TheNeutralElement(End(\<M>, A\<^sub>M), EndAdd(\<M>, A\<^sub>M))" .
-  from trans[OF this add_mult_neut_elems(2)] have "H`\<zero> = ConstantFunction(\<M>,\<Theta>)" .
+  from trans[OF vec_act_homo.homomor_dest_zero add_mult_neut_elems(2)] have "H`\<zero> = ConstantFunction(\<M>,\<Theta>)" by auto
   then have "(H`\<zero>)`g = ConstantFunction(\<M>,\<Theta>)`g" by auto
   then show "\<zero> \<cdot>\<^sub>S g=\<Theta>" using func1_3_L2 assms by auto
 qed
