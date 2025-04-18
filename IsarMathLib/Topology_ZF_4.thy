b12aaaa5099bb7e9d8edeea9dc7e956b1ed07bc7
dnl232 <danconsa@hotmail.com>
2025-04-18 21:34:05 +0100

    fix style

--- IsarMathLib/Topology_ZF_4.thy
+++ IsarMathLib/Topology_ZF_4.thy
@@ -401,7 +401,8 @@ proof
     unfolding SatisfiesFilterBase_def by blast
   let ?F = "{AA\<in>Pow(X). (\<exists>D\<in>{F\<inter>A. F\<in>\<FF>}. D\<subseteq>AA)}"
   have p:"{F\<inter>A. F\<in>\<FF>} \<subseteq> Pow(X)" using assms(2) unfolding IsFilter_def by auto
-  with ne have base:"{F\<inter>A. F\<in>\<FF>} {is a base filter} ?F" using base_unique_filter_set1(1)[of "{F\<inter>A. F\<in>\<FF>}" X]
+  with ne have base:"{F\<inter>A. F\<in>\<FF>} {is a base filter} ?F"
+    using base_unique_filter_set1(1)[of "{F \<inter> A . F \<in> \<FF>}"]
     by auto
   have "\<Union>?F =X" using base_unique_filter_set3(2)[OF p] baseCond by auto
   with base baseCond have "?F{is a filter on}X" using basic_filter by auto moreover
